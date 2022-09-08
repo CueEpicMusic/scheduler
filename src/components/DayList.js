9806443d864f7de99db1c9a6fd610bd9dbee86f4
Tom <chiatunglo@hotmail.com>
2022-09-07 20:38:49 -0400

    add daylist and daylistitem, test in storybook

---
+++ src/components/DayList.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import DayListItem from "components/DayListItem";
+
+export default function DayList(props) {
+  const days = props.days;
+  const daylist = days.map((day) => {
+    return <DayListItem key={day.id} {...day} selected={day.name===props.day} setDay={props.setDay}/>
+  })
+  return <>{daylist}</>
+}
